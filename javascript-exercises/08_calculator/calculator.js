aec7695372a957d1407bae2602c11b64b7555d12
Iagovar <iagovar@outlook.com>
2022-01-20 21:38:42 +0100

    Finalizados ejercicios de 08 calculator

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,72 @@
-const add = function() {
-	
-};
+const add = function sumatorio(...numbers) {
+	// Espera un número arbitario de números y genera una suma de todos ellos
 
-const subtract = function() {
-	
-};
+	suma = 0;
 
-const sum = function() {
-	
-};
+	for (i of numbers) {
+		suma += i;
+	}
 
-const multiply = function() {
+	return suma;
 
 };
 
-const power = function() {
+const subtract = function restando(...numbers) {
+	// Espera un número arbitario de números y genera una resta de todos ellos
+
+	resta = numbers[0] * 2;
+
+	for (i of numbers) {
+		resta -= i;
+	}
+
+	return resta;
+
+};
+
+const sum = function sumatorioArray(matriz) {
+	// Espera un número arbitario de números y genera una suma de todos ellos
+
+	suma = 0;
+
+	for (i of matriz) {
+		suma += i;
+	}
+
+	return suma;
+
+};
+
+const multiply = function multiplicar(matriz) {
+	// Espera un número arbitario de números y genera una multiplicación de todos ellos
+
+	multiplicados = 1;
+
+	for (i of matriz) {
+		multiplicados *= i;
+	}
+
+	return multiplicados;
+
+};
+
+const power = function potencia(base, exponente) {
+	return Math.pow(base, exponente);
 	
 };
 
-const factorial = function() {
-	
+const factorial = function nfactorial(n) {
+
+	let indice = 1;
+
+	let resultado = 1;
+
+	while (indice <= n) {
+		resultado *= indice;
+		indice++;
+	}
+
+	return resultado;
 };
 
 // Do not edit below this line
