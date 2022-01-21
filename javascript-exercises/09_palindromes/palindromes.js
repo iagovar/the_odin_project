e0017bf88a485d500b3dddee867dfbf6ccf63bd2
Iagovar <iagovar@outlook.com>
2022-01-21 14:08:03 +0100

    terminados palindromos

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function esPalindromo(cadena) {
+
+	const aminusculas = cadena.toLowerCase();
+	const sinpuntuacion = aminusculas.replace(/[^A-Z0-9]/ig, ""); // Eliminamos espacios y caracteres especiales
+    const matrizado = Array.from(sinpuntuacion);
+  
+	let alreves = [];
+	let j = 0;
+
+	for (let i = sinpuntuacion.length -1; i >= 0; i--) {
+		alreves[j] = matrizado[i];
+		j++;
+	};
+
+	const resultado = (matrizado.toString() == alreves.toString()) ? true : false;
+
+	return resultado;
+
 
 };
 
