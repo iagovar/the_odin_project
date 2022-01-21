7050895b4f36f3e23b8af5c6705eac3fa5a474eb
Iagovar <iagovar@outlook.com>
2022-01-21 19:27:01 +0100

    terminado fibonacci

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function fibonacciIndex(indice) {
+
+	// Sanitizando el imput
+
+	indice = Number(indice);
+	if (indice < 0) {return "OOPS"}
+
+	// Ejecutando el cálculo
+
+	let matriz = [1, 1];
+
+	for (let i = 0; i < indice; i++) {
+		if (i>1) {matriz[i] = matriz[i-1] + matriz[i-2]}
+	};
+	
+	return matriz[indice-1];
+  
 
 };
 
