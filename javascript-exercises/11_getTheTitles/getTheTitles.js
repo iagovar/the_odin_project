c2c0506ee690a75248860c7ab2ff797f4066f1c6
Iagovar <iagovar@outlook.com>
2022-01-23 20:57:16 +0100

    Finished getTheTitles, about js objects

--- javascript-exercises/11_getTheTitles/getTheTitles.js
+++ javascript-exercises/11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function titleReturner(booksArray) {
+	// Expects an array of objects, returns the "title" values in array trype
 
-};
+	const bookTitles = [];
+
+	for (index in booksArray) {
+	bookTitles.push(booksArray[index].title)
+	};
+
+	return bookTitles;
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
