7ba3d66c315f74723c988b51e7f80d805dd3a2fe
Iagovar <iagovar@outlook.com>
2022-02-01 14:31:01 +0100

    commit for changing to laptop

---
+++ javascript30challenge/01 - JavaScript Drum Kit/drumkit.js
@@ -0,0 +1,18 @@
+/*
+
+This script is NOT resembling the code in the JS30 challenge video
+
+*/
+//////////////////////////////////////////////////////////////////////
+// Main procedure
+//////////////////////////////////////////////////////////////////////
+
+
+// Putting all nodes in memory and adding the event listeners so we
+// avoid a messy html
+const buttons = document.querySelectorAll(".key");
+
+for (element of buttons) {
+	element.
+};
+
